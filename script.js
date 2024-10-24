e6d8157da369de3c502468cbae7f93c58d0df4bb
vinokr <vinokr0606@gmail.com>
2024-10-24 20:22:20 +0530

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-document.querySelector('.back-to-top').addEventListener('click', function(e) {
-    e.preventDefault();
-    window.scrollTo({
-        top: 0,
-        behavior: 'smooth'
-    });
-});
